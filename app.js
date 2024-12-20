fb44971c9924bfc5bb6feb2b7663fdab2d355da0
KatiFerrari <katielidasilvaferrari26@gmail.com>
2024-12-19 22:15:22 -0400

    series

--- app.js
+++ app.js
@@ -38,6 +38,17 @@ app.get('/pokemons', (req, res) => {
   res.json(pokemons);
 });
 
+app.use(express.json()); 
+app.post('/series', (req, res) => {
+    const series = [
+        "Breaking Bad",
+        "Game of Thrones",
+        "Stranger Things"
+    ];
+    res.json(series);
+});
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
